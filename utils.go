2a792c191c748d4cf37029d1d1760fe2ffb996ef
cooperq <cooperq@cooperq.com>
2020-06-25 11:26:12 -0700

    update rules and scanning PoC

---
+++ utils.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"regexp"
+
+	"github.com/hillu/go-yara"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+// Consts
+const awesomeListURL = "https://raw.githubusercontent.com/InQuest/awesome-yara/master/README.md"
+
+func updateAwesomelist() {
+	resp, err := http.Get(awesomeListURL)
+	db := openDB()
+	defer db.Close()
+	if err != nil {
+		log.Fatalln(err)
+		db := openDB()
+		defer db.Close()
+	}
+	if resp.Status != "200 OK" {
+		//body, _ := ioutil.ReadAll(resp.Body)
+		//fmt.Println(string(body))
+
+		log.Fatalln(resp.Status, "Could not fetch awesome yara")
+	}
+
+	scanner := bufio.NewScanner(resp.Body)
+	flag := false
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "## Rules" {
+			flag = true
+		} else if line == "## Tools" {
+			break
+		} else {
+			if flag {
+				re := regexp.MustCompile(`\[(.*)\]\((https://github.*)\)`)
+				match := re.FindStringSubmatch(line)
+				if len(match) < 3 {
+					// Not a successfull match so lets continue to the next line
+					continue
+				}
+
+				scanner.Scan()
+				Description := scanner.Text()
+				ruleset := Ruleset{Name: match[1], URL: match[2], Description: Description}
+				// Create or update ruleset in db
+				// Ping
+				db.Where(Ruleset{Name: ruleset.Name}).Assign(ruleset).FirstOrCreate(&ruleset)
+				rulesets = append(rulesets, ruleset)
+			}
+		}
+	}
+
+	resp.Body.Close()
+
+	fmt.Printf("Downloading %d rulesets...\n", len(rulesets))
+}
+
+// Exists returns whether a given path exists
+func Exists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
+
+// Warning prints a warning if there is a non fatal error
+func Warning(err error) {
+	if err != nil {
+		log.Printf("WARNING: %s", err)
+	}
+}
+
+func printMatches(m []yara.MatchRule, err error) {
+	if err == nil {
+		if len(m) > 0 {
+			for _, match := range m {
+				log.Printf("- [%s] %s ", match.Namespace, match.Rule)
+			}
+		} else {
+			log.Print("no matches.")
+		}
+	} else {
+		log.Printf("error: %s.", err)
+	}
+}
+
+// usage prints help about the program
+func usage() {
+	fmt.Print(""+
+		"YAYA - Yet Another Yara Automaton\n"+
+		"Usage:\n"+
+		os.Args[0], " [-h] <command> <path>\n"+
+		"\t-h\t print this help screen\n"+
+		"Commands:\n"+
+		"\tupdate - update rulesets\n"+
+		"\tedit - ban or remove rulesets\n"+
+		"\tadd - add a custom ruleset, located at <path>\n"+
+		"\tscan - perform a yara scan on the directory at <path>\n")
+	os.Exit(1)
+}
+
+func openDB() *gorm.DB {
+	db, err := gorm.Open("sqlite3", dbPath)
+	if err != nil {
+		panic("failed to connect database")
+	}
+	return db
+}
--- yaya.go
+++ yaya.go
@@ -5,134 +5,68 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"log"
-	"net/http"
 	"os"
 	"path"
-	"regexp"
+	"path/filepath"
+	"strings"
 
 	"github.com/go-git/go-git"
+	"github.com/hillu/go-yara"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
 // Ruleset is a record for a yara ruleset
 type Ruleset struct {
-	name        string
-	url         string
-	description string
-	enabled     bool
+	gorm.Model
+	Name        string `gorm:"unique_index"`
+	URL         string
+	Description string
+	Enabled     bool `gorm:"default:true"`
+	Rules       []Rule
+}
+
+// Rule is an individual YARA rule
+type Rule struct {
+	gorm.Model
+	Namespace string
+	Path      string
+	Enabled   bool `gorm:"default:true"`
+	RulesetID uint
 }
 
 var rulesets []Ruleset
+var rules []Rule
+
+// Paths
 var home, _ = os.UserHomeDir()
 var configdir = path.Join(home, ".yaya")
 var rulesetsdir = path.Join(configdir, "rulsets")
 
-func updateAwesomelist() {
-	var awesomeListURL = "https://raw.githubusercontent.com/InQuest/awesome-yara/master/README.md"
-	resp, err := http.Get(awesomeListURL)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	if resp.Status != "200 OK" {
-		//body, _ := ioutil.ReadAll(resp.Body)
-		//fmt.Println(string(body))
-
-		log.Fatalln(resp.Status, "Could not fetch awesome yara")
-	}
-
-	scanner := bufio.NewScanner(resp.Body)
-	flag := false
-	for scanner.Scan() {
-		line := scanner.Text()
-		if line == "## Rules" {
-			flag = true
-		} else if line == "## Tools" {
-			break
-		} else {
-			if flag {
-				re := regexp.MustCompile(`\[(.*)\]\((https://github.*)\)`)
-				match := re.FindStringSubmatch(line)
-				if len(match) < 3 {
-					// Not a successfull match so lets continue to the next line
-					continue
-				}
-
-				scanner.Scan()
-				description := scanner.Text()
-				r := Ruleset{match[1], match[2], description, true}
-				rulesets = append(rulesets, r)
-			}
-		}
-	}
-
-	resp.Body.Close()
-
-	fmt.Printf("Downloading %q rulesets...\n", len(rulesets))
-}
-
-func cloneRulesets() {
-	for _, ruleset := range rulesets {
-		fmt.Printf("git clone %q\n", ruleset.url)
-
-		_, err := git.PlainClone(path.Join(rulesetsdir, ruleset.name), false, &git.CloneOptions{
-			URL: ruleset.url,
-		})
-		if err != nil {
-			log.Println(err)
-			continue
-		}
-	}
-}
-
-// updateRules checks git repostitories for any new rules that have been added
-func updateRules() {
-	fmt.Println("Updating YARA Rules...")
-	updateAwesomelist()
-	cloneRulesets()
-}
-
-// editRules presents a UI allowing the user to ban certain rulesets or individual rules
-func editRules() {
-	fmt.Println("Editing YARA Rules...")
-}
-
-// addRules allows the user to add a ruleset from either a git repository or local file path
-func addRules(path string) {
-	fmt.Println("Adding YARA Rules from ", path)
-}
-
-// scan Scan a path recursively with every rule in the database
-func scan(path string) {
-	fmt.Println("Scanning at ", path)
-}
-
-// usage prints help about the program
-func usage() {
-	fmt.Print(""+
-		"YAYA - Yet Another Yara Automaton\n"+
-		"Usage:\n"+
-		os.Args[0], " [-h] <command> <path>\n"+
-		"\t-h\t print this help screen\n"+
-		"Commands:\n"+
-		"\tupdate - update rulesets\n"+
-		"\tedit - ban or remove rulesets\n"+
-		"\tadd - add a custom ruleset\n"+
-		"\tscan - perform a yara scan\n")
-	os.Exit(1)
-}
+// Database
+var dbPath = path.Join(configdir, "yaya.db")
 
 func main() {
 	if !(len(os.Args) >= 2) || os.Args[1] == "-h" {
 		usage()
 	}
 
+	db := openDB()
+	defer db.Close()
+
+	// Migrate the schema
+	db.AutoMigrate(&Rule{})
+	db.AutoMigrate(&Ruleset{})
+
 	command := os.Args[1]
-	var path string
+	var path string = ""
 	if len(os.Args) > 2 {
 		path = os.Args[2]
 	}
+
+	initYaya(db)
 
 	switch command {
 	case "update":
@@ -140,11 +74,192 @@ func main() {
 	case "edit":
 		editRules()
 	case "add":
+		if path == "" {
+			log.Fatalln("You must specifcy a ruleset path or github url to add.")
+		}
 		addRules(path)
 	case "scan":
-		scan(path)
+		if path == "" {
+			log.Fatalln("You must specifcy a path to scan.")
+		}
+		runScan(path)
 	default:
 		fmt.Println("Command not recognized")
 		usage()
+	}
+}
+
+//initYara populates the ruleset database with repos from the awesomelist
+func initYaya(db *gorm.DB) {
+	var count int
+	db.Table("rulesets").Count(&count)
+	if count > 1 {
+		return
+	}
+	fmt.Println("Running YAYA for the first time. Gathering initial rulesets.")
+	updateAwesomelist()
+	updateRules()
+}
+
+// updateRules checks git repostitories for any new rules that have been added
+func updateRules() {
+	fmt.Println("Updating YARA Rules...")
+	pullRulesets()
+	findRules()
+}
+
+func loadRulesets(rulesets *[]Ruleset) {
+	db := openDB()
+	defer db.Close()
+
+	db.Where("enabled = ?", true).Find(&rulesets)
+}
+
+func pullRulesets() {
+	loadRulesets(&rulesets)
+	for _, ruleset := range rulesets {
+		rulesetPath := path.Join(rulesetsdir, ruleset.Name)
+		pathExists, _ := Exists(rulesetPath)
+		if !pathExists {
+			fmt.Printf("git clone %q\n", ruleset.URL)
+
+			_, err := git.PlainClone(rulesetPath, false, &git.CloneOptions{
+				URL: ruleset.URL,
+			})
+			if err != nil {
+				log.Println(err)
+			}
+		} else {
+			fmt.Printf("git pull %s", ruleset.Name)
+			// We instantiate a new repository targeting the given path (the .git folder)
+			r, err := git.PlainOpen(rulesetPath)
+			Warning(err)
+
+			// Get the working directory for the repository
+			w, err := r.Worktree()
+			Warning(err)
+
+			// Pull the latest changes from the origin remote and merge into the current branch
+			err = w.Pull(&git.PullOptions{RemoteName: "origin"})
+			Warning(err)
+		}
+	}
+}
+
+func findRules() {
+	db := openDB()
+	defer db.Close()
+	loadRulesets(&rulesets)
+	for _, ruleset := range rulesets {
+		rulesetPath := path.Join(rulesetsdir, ruleset.Name)
+		pathExists, _ := Exists(rulesetPath)
+		if !ruleset.Enabled {
+			continue
+		}
+		fmt.Printf("scanning %q located at %s\n", ruleset.Name, rulesetPath)
+		if !pathExists {
+			Warning(fmt.Errorf("the ruleset path %s doesn't exist, try running an update", rulesetPath))
+			continue
+		}
+
+		// scan ruleset path for *.yar[a]?$
+		filepath.Walk(rulesetPath, func(path string, info os.FileInfo, e error) error {
+			if e != nil {
+				return e
+			}
+
+			// check if it is a regular file (not dir)
+			if info.Mode().IsRegular() {
+				matched, _ := filepath.Match(`*.yar*`, info.Name())
+				if matched {
+					// create struct Rule and append to ruleset
+					r := Rule{Path: path, RulesetID: ruleset.ID}
+					record := db.FirstOrCreate(&r, r)
+					rulename := strings.TrimSuffix(info.Name(), filepath.Ext(info.Name()))
+					r.Namespace = fmt.Sprintf("%s:%s-%d", ruleset.Name, rulename, r.ID)
+					db.Save(&r)
+					// validate yara rule
+					if r.Enabled {
+						c, _ := yara.NewCompiler()
+						f, err := os.Open(r.Path)
+						if err != nil {
+							log.Printf("Could not open rule file %s: %s\n", r.Path, err)
+							return nil
+						}
+						err = c.AddFile(f, r.Namespace)
+						f.Close()
+						if err != nil {
+							log.Printf("Could not parse rule file %s: %s", r.Path, err)
+							r.Enabled = false
+							db.Save(&r)
+						}
+					}
+
+					db.Model(&ruleset).Association("Rules").Append(record)
+				}
+			}
+			return nil
+		})
+		// DB update ruleset
+		db.Save(&ruleset)
+	}
+}
+
+// editRules presents a UI allowing the user to ban certain rulesets or individual rules
+func editRules() {
+	loadRulesets(&rulesets)
+	fmt.Println("Editing YARA Rules.")
+}
+
+// addRules allows the user to add a ruleset from either a git repository or local file path
+func addRules(path string) {
+	fmt.Println("Adding YARA Rules from ", path)
+}
+
+// runScan Scan a path recursively with every rule in the database
+func runScan(path string) {
+	loadRulesets(&rulesets)
+	db := openDB()
+	defer db.Close()
+
+	//db.Where("enabled = ?", true).Find(&rulesets)
+	for _, ruleset := range rulesets {
+		c, err := yara.NewCompiler()
+		if err != nil {
+			log.Fatalf("Failed to initialize YARA compiler: %s", err)
+		}
+		db.Where("enabled = ?", true).Model(&ruleset).Related(&rules)
+		fmt.Printf("Ruleset %s scanning with %d rules\n", ruleset.Name, len(rules))
+		for _, rule := range rules {
+			f, err := os.Open(rule.Path)
+			if err != nil {
+				log.Printf("Could not open rule file %s: %s\n", rule.Path, err)
+			}
+			err = c.AddFile(f, rule.Namespace)
+			f.Close()
+			if err != nil {
+				log.Printf("Could not parse rule file %s: %s", rule.Path, err)
+				break
+			}
+		}
+		rules, err := c.GetRules()
+		if err != nil {
+			log.Printf("Failed to compile rules: %s", err)
+			continue
+		}
+
+		var matches yara.MatchRules
+
+		log.Printf("Scanning file %s... ", path)
+		matches, err = rules.ScanFile(path, 0, 0)
+		printMatches(matches, err)
+		//fmt.Printf("Rules: %q\n\nMatches: %q", rules, matches)
+		/*
+			for _, filename := range args {
+				log.Printf("Scanning file %s... ", path)
+				err := rules.ScanFile(path, 0, 0, &m)
+				printMatches(m, err)
+			}
+		*/
 	}
 }
